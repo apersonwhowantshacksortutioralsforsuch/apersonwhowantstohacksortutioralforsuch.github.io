dfa73a6443e78a9fb494abc7c2ccc7529a8eb067
apersonwhowantshacksortutioralsforsuch <106562789+apersonwhowantshacksortutioralsforsuch@users.noreply.github.com>
2024-02-03 12:10:08 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,57 +1,70 @@
-let ball;
-let gravity;
-let ballSpeedVert;
-let ballSize;
-let ballColor;
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+const canvasWidth = 600;
+const canvasHeight = 400;
+canvas.width = canvasWidth;
+canvas.height = canvasHeight;
 
-function setup() {
-  createCanvas(500, 500);
-  ballX = width / 4;
-  ballY = height / 5;
-  ballSize = 20;
-  ballColor = color(0);
-  gravity = 1;
-  ballSpeedVert = 0;
+const ball = {
+  x: canvasWidth / 2,
+  y: canvasHeight / 2,
+  radius: 15,
+  speed: 3,
+  velX: 0,
+  velY: 0,
+};
+
+function drawCircle(x, y, radius, color) {
+  ctx.beginPath();
+  ctx.arc(x, y, radius, 0, Math.PI * 2, false);
+  ctx.fillStyle = color;
+  ctx.fill();
 }
 
-function draw() {
-  background(255);
-  applyGravity();
-  keepInScreen();
-  drawBall();
-}
+function update() {
+  ball.x += ball.velX;
+  ball.y += ball.velY;
 
-function drawBall() {
-  fill(ballColor);
-  ellipse(ballX, ballY, ballSize, ballSize);
-}
-
-function applyGravity() {
-  ballSpeedVert += gravity;
-  ballY += ballSpeedVert;
-}
-
-function makeBounceBottom(surface) {
-  ballY = surface - (ballSize / 2);
-  ballSpeedVert *= -1;
-}
-
-function makeBounceTop(surface) {
-  ballY = surface + (ballSize / 2);
-  ballSpeedVert *= -1;
-}
-
-function keepInScreen() {
-  if (ballY + (ballSize / 2) > height) {
-    makeBounceBottom(height);
+  if (ball.x + ball.radius > canvasWidth) {
+    ball.velX = -ball.speed;
+  } else if (ball.x - ball.radius < 0) {
+    ball.velX = ball.speed;
   }
-  if (ballY - (ballSize / 2) < 0) {
-    makeBounceTop(0);
+
+  if (ball.y + ball.radius > canvasHeight) {
+    ball.velY = -ball.speed;
+  } else if (ball.y - ball.radius < 0) {
+    ball.velY = ball.speed;
   }
+
+  drawCircle(ball.x, ball.y, ball.radius, 'black');
 }
 
-function mousePressed() {
-  if (mouseY > ballY - ballSize / 2 && mouseY < ballY + ballSize / 2) {
-    ballColor = color(random(255), random(255), random(255));
+function keyDown(e) {
+  if (e.key === 'ArrowUp') {
+    ball.velY = -ball.speed;
+  } else if (e.key === 'ArrowDown') {
+    ball.velY = ball.speed;
+  } else if (e.key === 'ArrowLeft') {
+    ball.velX = -ball.speed;
+  } else if (e.key === 'ArrowRight') {
+    ball.velX = ball.speed;
   }
 }
+
+function keyUp(e) {
+  if (e.key === 'ArrowUp' || e.key === 'ArrowDown') {
+    ball.velY = 0;
+  } else if (e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
+    ball.velX = 0;
+  }
+}
+
+function main() {
+  update();
+  requestAnimationFrame(main);
+}
+
+document.addEventListener('keydown', keyDown);
+document.addEventListener('keyup', keyUp);
+main();
